45d72eea78f2ec58480034a031f29c9daddcd03a
tougshire <87095019+tougshire@users.noreply.github.com>
2021-07-07 18:09:33 -0400

    IO Board for the Suffolk Public LIbrary

---
+++ functions/test.js
@@ -0,0 +1,25 @@
+// Grab our credentials from a .env file or environment variables
+require('dotenv').config();
+const {
+	DATABASE_URL,
+	SUPABASE_SERVICE_API_KEY
+} = process.env;
+
+// Connect to our database 
+const { createClient } = require('@supabase/supabase-js');
+const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
+
+// Our standard serverless handler function
+exports.handler = async event => {
+
+  // Insert a row
+	const { data, error } = await supabase
+		.from('IOBoard')
+ 		.insert([
+ 			{ name: 'Kyle Binaxas',Status: true },
+		]);
+
+  // Did it work?
+  console.log(data, error);
+  
+}
