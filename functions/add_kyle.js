b5f323ff88ac39bfb5128bb637dc9a8d3fb2b07d
tougshire <87095019+tougshire@users.noreply.github.com>
2021-07-08 12:31:47 -0400

    Update add_kyle.js

--- functions/add_kyle.js
+++ functions/add_kyle.js
@@ -5,7 +5,6 @@ require('dotenv').config()
 const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient(process.env.DATABASE_URL, process.env.SUPABASE_SERVICE_API_KEY);
 
-
 // Our standard serverless handler function
 exports.handler = async event => {
 
