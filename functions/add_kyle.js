fcc5cfa71c18bc9c76b32e55d0378f90876015f3
tougshire <87095019+tougshire@users.noreply.github.com>
2021-07-08 12:00:37 -0400

    added require dotenv and process.env in add_kyle.js

--- functions/add_kyle.js
+++ functions/add_kyle.js
@@ -1,8 +1,10 @@
 // Grab our credentials from a .env file or environment variables
+require('dotenv').config()
 
 // Connect to our database 
 const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
+const supabase = createClient(process.env.DATABASE_URL, process.env.SUPABASE_SERVICE_API_KEY);
+
 
 // Our standard serverless handler function
 exports.handler = async event => {
